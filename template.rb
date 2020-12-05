c8984408d9418f8603ba59509e872721bfa468ae
Thomas Van Holder <thomasvanholder@hey.com>
2020-12-05 16:41:51 +0100

    update style

--- template.rb
+++ template.rb
@@ -41,8 +41,10 @@ def add_tailwind
     end
 
   run "npx tailwindcss init --full"
-  gsub_file "tailwind.config.js", /plugins:\s\[],/, "plugins: [require('@tailwindcss/ui'),require('@tailwindcss/typography'),
-    require('@tailwindcss/aspect-ratio')],"
+  gsub_file "tailwind.config.js", /plugins:\s\[],/, "plugins:
+    \n[require('@tailwindcss/ui'),
+    \nrequire('@tailwindcss/typography'),
+    \nrequire('@tailwindcss/aspect-ratio')],"
   run "mv tailwind.config.js app/javascript/stylesheets/tailwind.config.js"
 
   append_to_file("app/javascript/packs/application.js", 'import "stylesheets/application"')
