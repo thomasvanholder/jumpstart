fc3d16d8a50f9ad8546167ce869388f21f4576b3
Thomas Van Holder <thomasvanholder@hey.com>
2020-12-05 15:41:05 +0100

    new up

--- template.rb
+++ template.rb
@@ -1,6 +1,7 @@
 run "if uname | grep -q 'Darwin'; then pgrep spring | xargs kill -9; fi"
 
 # GEMFILE
+
 inject_into_file 'Gemfile', before: 'group :development, :test do' do
   <<~RUBY
     gem 'devise'
@@ -11,9 +12,9 @@ end
 
 inject_into_file 'Gemfile', after: 'group :development, :test do' do
   <<-RUBY
-    gem 'pry-byebug'
-    gem 'pry-rails'
-    gem 'dotenv-rails'
+  gem 'pry-byebug'
+  gem 'pry-rails'
+  gem 'dotenv-rails'
   RUBY
 end
 
